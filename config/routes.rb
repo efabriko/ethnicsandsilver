be35d9127678ac616ad39aa53bb715b06e201822
eFabriko <efabriko@gmail.com>
2020-11-17 19:16:52 +0700

    3.8 created about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # Below line will map any request to get the about page to the about action of the simple_pages controller
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+
+  get 'simple_pages/index'
+  # Thanks to this routes.rb and below line, Rails will map requests to the root of the App to the to the index action of the simple_pages controller
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
