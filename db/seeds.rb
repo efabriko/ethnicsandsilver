497f88d80dde4596fbaf8556df65a84b9b4578bd
eFabriko <efabriko@gmail.com>
2020-11-24 20:44:55 +0700

    4.6 Updated Orders controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,22 @@
 
 # Below some inputs to seed our products database
 # Use $ rails db:seed in dev. environment and $ heroku run rake db:seed in production environment
-products = Product.create([
-  { name: "Bracelet", description: "Nice bracelet 1", image_url: "products-img/bracelet_1.jpg" },
-  { name: "Bracelet", description: "Nice bracelet 2", image_url: "products-img/bracelet_2.jpg" },
-  { name: "Earrings", description: "Nice Earrings 1", image_url: "products-img/earrings_1.jpg" },
-  { name: "Earrings", description: "Nice Earrings 2", image_url: "products-img/earrings_2.jpg" },
-  { name: "Necklace", description: "Nice necklace 1", image_url: "products-img/necklace_1.jpg" },
-  { name: "Necklace", description: "Nice necklace 2", image_url: "products-img/necklace_2.jpg" },
-  { name: "Necklace", description: "Nice necklace 3", image_url: "products-img/necklace_3.jpg" },
-  { name: "Ring", description: "Nice ring 1", image_url: "products-img/ring_1.jpg" },
-  { name: "Ring", description: "Nice ring 2", image_url: "products-img/ring_2.jpg" }
+# products = Product.create([
+#   { name: "Bracelet", description: "Nice bracelet 1", image_url: "products-img/bracelet_1.jpg" },
+#   { name: "Bracelet", description: "Nice bracelet 2", image_url: "products-img/bracelet_2.jpg" },
+#   { name: "Earrings", description: "Nice Earrings 1", image_url: "products-img/earrings_1.jpg" },
+#   { name: "Earrings", description: "Nice Earrings 2", image_url: "products-img/earrings_2.jpg" },
+#   { name: "Necklace", description: "Nice necklace 1", image_url: "products-img/necklace_1.jpg" },
+#   { name: "Necklace", description: "Nice necklace 2", image_url: "products-img/necklace_2.jpg" },
+#   { name: "Necklace", description: "Nice necklace 3", image_url: "products-img/necklace_3.jpg" },
+#   { name: "Ring", description: "Nice ring 1", image_url: "products-img/ring_1.jpg" },
+#   { name: "Ring", description: "Nice ring 2", image_url: "products-img/ring_2.jpg" }
+# ])
+
+
+# Below some inputs to seed our orders database
+# Use $ rails db:seed in dev. environment and $ heroku run rake db:seed in production environment
+products = Order.create([
+  { user_id: 1, product_id: 12, total: 12.4 },
+  { user_id: 2, product_id: 14, total: 25.6 }
 ])
