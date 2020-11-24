233ff006d8e10540f2b6a458de04e613d77d64ec
eFabriko <efabriko@gmail.com>
2020-11-24 09:32:03 +0700

    setup carousel in landing page, created and setup partial for product index view

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,6 +3,11 @@ class SimplePagesController < ApplicationController
   end
 
   def landing_page
-    @featured_product = Product.first
+    products = Product.all
+    if products.size <= 3
+      @products = Product.order("created_at desc").where.not(image_url: nil).limit(3)
+    else
+      @products = Product.order("created_at desc").where.not(image_url: nil).limit(5)
+    end
   end
 end
