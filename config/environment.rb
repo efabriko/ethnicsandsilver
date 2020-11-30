24a2c396d3b6b9c99b28680aefb0989a1d4612a3
eFabriko <efabriko@gmail.com>
2020-11-30 22:30:54 +0700

    added mailgun functionality

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Enable ActionMailer to use SendGrid plugin to send out emails from Heroku
+# Enable ActionMailer to use Mailgun plugin to send out emails from Heroku
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: '587',
-  authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: 'heroku.com',
-  enable_starttls_auto: true
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain,
 }
+ActionMailer::Base.delivery_method = :smtp
