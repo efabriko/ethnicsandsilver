9b637913d6bbb782fcf4ebfa9508ddf702b4c86f
eFabriko <efabriko@gmail.com>
2020-12-07 21:35:06 +0700

    Created Comment Model scopes and Custom methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_125138) do
+ActiveRecord::Schema.define(version: 2020_12_07_132344) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "body"
+    t.integer "rating"
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -45,4 +56,6 @@ ActiveRecord::Schema.define(version: 2020_12_02_125138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
 end
